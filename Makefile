597f61648f42ec7e84cc916fae1a85658a9b230c
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-05 02:49:58 +0100

    push

--- Makefile
+++ Makefile
@@ -1,12 +1,30 @@
 # build using docker comnpose
 
 
-# UP
 
-# DOWN
 
-git :
+all:
+
+run:
+
+up:
+	docker-compose up
+
+down:
+	docker-compose down
+
+debug:
+
+list:
+
+volumes:
+
+clean:
+
+git:
 	git add .
 	git status
 	git commit -m "push"
 	git push
+
+.PHONY:
---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mysql_install_db
+
+mysqld_safe 
+
+sleep 5
+
+echo  "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" \
+     "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" "FLUSH PRIVILEGES;" | mysql -u root -p${MYSQL_ROOT_PASSWORD}
+
+mysqladmin shutdown -p${MYSQL_ROOT_PASSWORD}
+
+mysqld_safe
