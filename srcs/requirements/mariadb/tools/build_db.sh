6f09bbeeb2a10342d8d3d9c8b8a585d1d2d1d5a5
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-06 21:33:46 +0100

    push

--- srcs/requirements/mariadb/tools/build_db.sh
+++ srcs/requirements/mariadb/tools/build_db.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+mysql -V
+
+# Bind address to 0.0.0.0 to allow connections from any IP address
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+#add data base location to mariadb my.cnf file to specify the mounted volume
+#
+
+# Start MariaDB service
+    # mysqld_safe  & or 
 service mariadb start
 
-# tape fix ping the database after to check if its up or not
-sleep 4
+# Wait for MariaDB to start
+sleep 10
 
-# if [ -d "/var/lib/mysql/$MARIADB_DATABASE" ]
-# then 
-# 	echo "database already exists !!! we gone use it ."
-# else
-
+# Run mysql_secure_install script
 ./tools/install.exp
 
+# Create database, grant privileges, and alter root password
+mysql -u root -e "
+CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\`;
+GRANT ALL ON \`$MARIADB_DATABASE\`.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';
+FLUSH PRIVILEGES;"
+
+# Stop MariaDB service
+service mariadb stop
+
+mysqld_safe
