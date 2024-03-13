701d436a6c9c5515b5b6393593b534bf97e0e480
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-13 05:34:25 +0000

    push

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -4,8 +4,9 @@
 
 # openssl x509 -in /etc/ssl/certs/nginx-selfsigned.crt -text -noout
 
-rm -rf /etc/nginx/sites-available/* /etc/nginx/sites-enabled/* /etc/nginx/ginx.conf
+rm -rf /etc/nginx/sites-enabled/*
 
-mv ../conf/nginx.conf /etc/nginx
+mv /nginx/conf/nginx.conf /etc/nginx/sites-enabled
 
 nginx -g 'daemon off;'
+
