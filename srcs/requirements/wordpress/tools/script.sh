97b6da46a362b180881061896c76a70ed52ece7d
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-14 04:33:40 +0000

    push

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,9 +2,9 @@
 
 wp core download --allow-root
 
-wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --allow-root
+wp core install --url=ebennix.42.fr --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com  --skip-email --allow-root
 
-wp core install --url=ebennix.42.fr --title=ebennix.42.fr --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
+wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --allow-root
 
 wp user create ebennix info@gmail.com --role=author --user_pass=$MARIADB_PASSWORD --allow-root
 
