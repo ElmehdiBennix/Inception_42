73076da27650e0b558d2c3c8f798e146198d083a
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-17 04:19:19 +0000

    autopush

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -10,7 +10,9 @@ echo "download finished \n"
 wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --allow-root
 echo "config create finished \n"
 
-wp core install --url=ebennix.42.fr --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
+wp core install --url=10.11.100.156 --title=leprissa --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
+# wp core install --url= --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
+# wp core install --url=ebennix.42.fr --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
 echo "core install finished \n"
 
 wp user create ebennix info@gmail.com --role=author --user_pass=$MARIADB_PASSWORD --allow-root
