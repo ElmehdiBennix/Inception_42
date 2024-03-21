3abd550b57c350e2e36f2b3e59c696c93feb7d89
ElmehdiBennix <bennixmehdi@gmail.com>
2024-03-21 08:31:36 +0000

    autopush

---
+++ srcs/requirements/bonus/ftp/tools/init_ftp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chown -R ftp_agent:ftp_agent /var/www/html
+
+chmod -R 755 /var/www/html
+
+# This enables FTP commands which change the filesystem, including commands like `STOR`, `DELE`, `RNFR`, `RNTO`, etc.
+echo "write_enable=YES" >> /etc/vsftpd.conf
+# This enables local users to log in to the FTP server.
+# echo "local_enable=YES" >> /etc/vsftpd.conf
+# This confines local users to their home directories by default, preventing them from navigating the entire filesystem.
+# echo "chroot_local_user=YES" >> /etc/vsftpd.conf
+# echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+# enable ftp passive and ports
+echo "pasv_enable=YES" >> /etc/vsftpd.conf
+echo "pasv_min_port=30000" >> /etc/vsftpd.conf
+echo "pasv_max_port=30009" >> /etc/vsftpd.conf
+
+exec "$@"
