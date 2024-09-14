819c4ab560ebd529d0732c3cbf652bb146e42ccb
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-14 09:39:29 +0100

    autopush

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
 
-sleep 5
+sleep 3
 
 sh /execute/sync.sh
 
-wp core download --allow-root
-echo "download finished \n"
-
-wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --allow-root
-
-# wp config create --ftphost=ftp --ftuser=$FTP_USER --ftppass=$FTP_PASSWORD --allow-root
-
-echo "config create finished \n"
-
-
-wp core install --url=10.11.100.156 --title=leprissa --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
-# wp core install --url=localhost --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
-# wp core install --url=ebennix.42.fr --title=mysite --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=bennixmehdi@gmail.com --allow-root
-
-echo "core install finished \n"
-
-wp user create ebennix info@gmail.com --role=author --user_pass=$MARIADB_PASSWORD --allow-root
-
-echo "user creatation finished \n"
-
-
-wp config set FTP_HOST ftp --allow-root
-wp config set FTP_USER ftp_agent --allow-root
-wp config set FTP_PASS ftp_agent1234 --allow-root
-# echo "\ndefine('FTP_HOST', 'ftp');\ndefine('FTP_USER', 'ftp_agent');\ndefine('FTP_PASS', 'ftp_agent1234');" >> /var/www/html/wordpress/wp-config.php
-
-wp plugin update --all --allow-root
-
-wp plugin install redis-cache --activate --allow-root
-
-wp config set WP_REDIS_HOST Redis_cache --allow-root
-wp config set WP_REDIS_PORT 6379 --allow-root
-
-wp redis enable --allow-root
-
 sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 9000|g" /etc/php/8.2/fpm/pool.d/www.conf
 
-echo "starting php fast procces manager \n"
+echo "Starting WP download..."
+wp core download --allow-root
+
+echo "Starting WP configuration ..."
+wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --allow-root
+wp core install --url=localhost --title=leprissa --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASSWORD --admin_email=$WP_EMAIL --allow-root
+wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
+
+# ftp config
+wp config set FTP_HOST ftp --allow-root
+wp config set FTP_USER $FTP_USER --allow-root
+wp config set FTP_PASS $FTP_PASSWORD --allow-root
+
+# update plugins
+wp plugin update --all --allow-root
+
+# redis config
+wp plugin install redis-cache --activate --allow-root
+wp config set WP_REDIS_HOST redis_cache --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp redis enable --allow-root
+
+echo "Starting PHP Fast Procces Manager:"
 exec "$@"
-
-
--- srcs/requirements/wordpress/tools/sync.sh
+++ srcs/requirements/wordpress/tools/sync.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-server="mariadb"
+container="mariadb"
 port="3306"
 
-server_status()
+service_status()
 {
-    nc -zv $server $port > /dev/null 2>&1
+    nc -zv $container $port > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "$server on $port is up\n"
         return 0;
     else
-        echo "$server on $port is down\n"
+        echo "Waiting for $container on $port..."
         return 1;
     fi
 }
 
-while ! server_status; do
-    sleep 2
+while ! service_status; do
+    sleep 0.5
 done
 
-echo "$server on $port is up and ready to be connected to :"
+echo "<=== Starting wordpress Execution ===>"
