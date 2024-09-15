0d08a28acc13262cd7e3c48a805f22311a49e552
ElmehdiBennix <bennixmehdi@gmail.com>
2024-09-15 07:22:37 +0100

    autopush

--- srcs/requirements/bonus/adminer/tools/sync.sh
+++ srcs/requirements/bonus/adminer/tools/sync.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-server="mariadb"
+container="mariadb"
 port="3306"
 
-server_status()
+service_status()
 {
-    nc -zv $server $port > /dev/null 2>&1
+    nc -zv $container $port > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "$server on $port is up\n"
         return 0;
     else
-        echo "$server on $port is down\n"
+        echo "Waiting for $container on $port..."
         return 1;
     fi
 }
 
-while ! server_status; do
-    sleep 2
+while ! service_status; do
+    sleep 0.5
 done
 
-echo "$server on $port is up and ready to be connected to :"
+echo "<=== adminer is up and running ===>"
 
 exec "$@"
-
-#no need nginx wont deliver unless mariadb is up
